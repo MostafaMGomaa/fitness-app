f10bfa26e04e1d6d9098c1040fede3afeb0a16d4
MostafaMGomaa <gomaamostafa26@gmail.com>
2023-02-09 18:39:14 +0200

    test

--- server/server.ts
+++ server/server.ts
@@ -6,5 +6,5 @@ import { app } from './app';
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`Server is running on PORT ${PORT}`);
+  console.log(`Server is running on PORT :${PORT}`);
 });
