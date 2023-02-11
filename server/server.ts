df00c0f27b9a33b48b777ad426c50556951ea38e
MostafaMGomaa <gomaamostafa26@gmail.com>
2023-02-11 20:35:28 +0200

    test sql connection

--- server/server.ts
+++ server/server.ts
@@ -1,12 +1,20 @@
 require('dotenv').config({ debug: true });
 
-import express from 'express';
 import { app } from './app';
+import { Sequelize } from 'sequelize-typescript';
 
 (async () => {
   const PORT = process.env.PORT || 3000;
+  const sequelize = new Sequelize({
+    username: process.env.POSTGRES_USERNAME,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DATABASE,
+    host: process.env.POSTGRES_HOST,
 
+    dialect: 'postgres',
+    storage: ':memory:',
+  });
   app.listen(PORT, () => {
-    console.log(`Server is running on PORT :${PORT}`);
+    console.log(`Server is running on PORT: ${PORT}`);
   });
 })();
